f5d4ee840de24f00a5295e0815a4bf9d01f9ce2c
Chris Bucholz <chrisbucholz@gmail.com>
2022-10-07 10:29:23 -0700

    added the depeendency, yarn.lock, .gitignore

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export default someFunctionWithADependency = () => {
+import testDependency from "yarn_upgrade_test_dependency";
+
+const someFunctionWithADependency = () => {
     console.log('gonna do things here that rely on another repo');
-}
+    testDependency();
+};
+
+someFunctionWithADependency();
+export default someFunctionWithADependency
